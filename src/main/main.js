f86196e74c7b362dae60cbeb215a31750ed008b6
Steve Hommy <steve96hommy@gmail.com>
2023-02-01 22:22:47 +0200

    Main file completed

---
+++ src/main/main.js
@@ -0,0 +1,23 @@
+const { calculateDistancesBetweenNetworkStationsAndDevice,
+    calculateSpeedsOfNetworkStationsWithRespectToDevice,
+    findBestNetworkStationForDevice
+} = require('./suitableNetworkStationForDevice.cjs');
+
+const devices = [
+    [100, 100],
+    [15, 10],
+    [18, 18],
+    [13, 13],
+    [25, 99]
+];
+
+const suitableNetworkStationForDevice = () => {
+    for (let i = 0; i < devices.length; i++) {
+        const distanceBetweenNetworkStationsAndDevice = calculateDistancesBetweenNetworkStationsAndDevice(devices[i][0], devices[i][1]);
+        const speedOfNetworkStationsWithRespectToDevice = calculateSpeedsOfNetworkStationsWithRespectToDevice(distanceBetweenNetworkStationsAndDevice)
+        findBestNetworkStationForDevice(devices[i][0], devices[i][1], speedOfNetworkStationsWithRespectToDevice)
+    };
+    return findBestNetworkStationForDevice
+}
+
+suitableNetworkStationForDevice()
